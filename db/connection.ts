0e94907d279649c5bc7b54d4664cda6fb7636638
Ronald <=>
2023-04-21 17:47:30 -0500

    configuracion en la base de datos

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('bd_portafolio', 'root', '', {
-  host: 'localhost',
+const db = new Sequelize( process.env.DB_NAME || 'bd_portafolio', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '', {
+  host: process.env.DB_HOST || 'localhost',
   dialect: 'mysql'
 });
 
