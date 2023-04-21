9ee148d9c6dbb829022568fcfa3917aadfe445a5
Ronald <=>
2023-04-21 18:04:22 -0500

    configurar puerto base de datos

--- db/connection.ts
+++ db/connection.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
-
+const port: number = Number(process.env.DB_PORT) || 3306
 const db = new Sequelize( process.env.DB_NAME || 'bd_portafolio', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '', {
   host: process.env.DB_HOST || 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  port
 });
 
 export default db;
